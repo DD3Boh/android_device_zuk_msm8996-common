62d8b8908bf2e3da437095a29413efa1a2500a40
Davide Garberi <dade.garberi@gmail.com>
2017-11-15 21:51:33 +0100

    [DO NOT MERGE] msm8996-common: Disable healthd
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -69,7 +69,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 # Healthd
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service

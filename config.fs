9ae5dd10c94c95bad91d257496a6f813ab30d056
Davide Garberi <dade.garberi@gmail.com>
2017-12-13 20:19:01 +0100

    msm8996-common: Update radio stack from LA.UM.6.5.r1-02500-8x96.0 [2/2]
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- config.fs
+++ config.fs
@@ -25,13 +25,25 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
